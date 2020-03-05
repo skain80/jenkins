8323625f7719d674a01ba841c76a624e5beb5929
skiznod <61068525+skiznod@users.noreply.github.com>
2020-03-05 16:30:13 +0000

    adding date

--- jobsBuildTimes.groovy
+++ jobsBuildTimes.groovy
@@ -1,27 +1,35 @@
 //println(Jenkins.instance.metaClass.methods.name.sort().unique())
 //Jenkins.instance.getJobNames()
+//Hudson.instance.items.get(40).lastBuild.time
 
 
 jobs = ["buildable":[],"nonbuildable":[]]
+
+
+Date now = new Date()
+Date builddate
 
 for (item in Hudson.instance.items)
 {
   if(!item.name.contains("Admin")) //omit com.cloudbees.hudson.plugins.folder.Folder
   {
-    if(!item.buildable)
+    if(item.buildable)
     {
-      //println(item.name + " " + "\"" + item.lastBuild + "\"");
-      if(item.lastBuild != null && item.lastBuild.time != "")
-      {
-        println(item.name + " " + item.lastBuild.time);
-        jobs["buildable"].add(item.name)
-      }
-      else
-      {
-        println(item.name + " " + "Never Built")
-        jobs["nonbuildable"].add(item.name)
-      }
+        //println(item.name + " " + "\"" + item.lastBuild + "\"");
+        if(item.lastBuild != null && item.lastBuild.time != "")
+        {
+            builddate = item.lastBuild.time
+          //println(item.name + " " + item.lastBuild.time);
 
+          jobs["buildable"].add(item.name)
+        }
+        else
+        {
+          println(item.name + " " + "Never Built")
+          jobs["nonbuildable"].add(item.name)
+        }
+        builddate += 357 //add +1year to last build
+        println(item.name + " " + builddate.after(now)) // true if in the future
     }
   }
 }
