02f87016fe2b72bc63a8a743733798a5da2b4c8a
skiznod <61068525+skiznod@users.noreply.github.com>
2020-03-09 10:27:01 +0000

    csv format and days

--- jobsBuildTimes.groovy
+++ jobsBuildTimes.groovy
@@ -8,31 +8,25 @@ jobs = ["buildable":[],"nonbuildable":[]]
 Date now = new Date()
 Date builddate
 
-for (item in Hudson.instance.items)
-{
-  if(!item.name.contains("Admin")) //omit com.cloudbees.hudson.plugins.folder.Folder
-  {
-    if(item.buildable)
-    {
-        //println(item.name + " " + "\"" + item.lastBuild + "\"");
-        if(item.lastBuild != null && item.lastBuild.time != "")
-        {
-            builddate = item.lastBuild.time
-          //println(item.name + " " + item.lastBuild.time);
+for (item in Hudson.instance.items) {
+  
+  if(!item.name.contains("Admin")) { //omit com.cloudbees.hudson.plugins.folder.Folder
+    
+    if(item.buildable) {
+     	if(item.lastBuild != null && item.lastBuild.time != "") {
+			builddate = item.lastBuild.time
+        }
+        else {
+          	println(item.name + ",Never," + Hudson.instance.rootUrl + item.url)
+          	continue
+        }
+      
+      	days_since_last_build = now - builddate
 
-          	jobs["buildable"].add(item.name)
+      
+      	if(days_since_last_build >= 356) {
+        	println(item.name + ","+days_since_last_build+"," + Hudson.instance.rootUrl + item.url)
         }
-        else
-        {
-          	println(item.name + " " + "Never Built")
-          	jobs["nonbuildable"].add(item.name)
-        }
-        builddate += 357 //add +1year to last build
-      	if(!builddate.after(now))
-      	{	
-        	println(item.name + " has not been built for 356+ days!")
-        }
-        
     }
   }
 }
